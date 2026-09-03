a30a8b81803aed2651effcc1e4fb7c7562df14e0
Azalea Hanin <azaleahanin6@gmail.com>
2026-09-03 09:02:33 +0700

    latihan c++

---
+++ iseng.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+cout << "soal 1" << endl;
+    string nama = "Azalea";
+    int umur = 18;
+    float tinggi_badan = 157;
+    bool apakah_sudah_menikah = 0;
+
+    cout << " nama : " << nama << endl;
+    cout << " umur : " << umur << endl;
+    cout << " tinggi badan : " << tinggi_badan << endl;
+    cout << " apakah sudah menikah : " << apakah_sudah_menikah << endl;
+
+cout << endl;
+
+cout << "soal 2" << endl;
+    int nilai_UTS = 90;
+    int nilai_UAS = 85;
+    int nilai_tugas = 90;
+
+    double hasil = (nilai_UTS * 0.3) + (nilai_UAS * 0.4) + (nilai_tugas * 0.3);
+    cout << hasil << endl;
+
+}       
---
+++ iseng2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double item_price;
+    int quantity;
+    double subtotal;
+    double discount;
+    double final_price;
+
+    cout << "Enter item price:  ";
+    cin >> item_price;
+
+cout << " Enter quantity: ";
+cin >> quantity;
+
+subtotal = (item_price * quantity);
+cout << subtotal << endl;
+
+if (subtotal > 100);
+    discount = (subtotal * 0,1);
+cout << " Congratulations! You get a 10% discount. " << endl;
+
+else ();
+
+cout << "No discount applied. " << endl;
+final_price = (subtotal - discount)
+
+cout << "subtotal: " << subtotal << endl
+cout << "discount: " << discount << endl
+cout << "total price: " << total price << endl
+    
+return 0;
+}
